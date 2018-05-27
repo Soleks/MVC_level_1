bfe63091609f6e2bb737a17b4c70ec972e323649
Soleks <SolekX@gmail.com>
2018-05-27 14:29:16 +0300

    Lesson_2: Add View, PartialView and Action

--- Lesson_2/WebStore/WebStore/Controllers/HomeController.cs
+++ Lesson_2/WebStore/WebStore/Controllers/HomeController.cs
@@ -17,5 +17,45 @@ namespace WebStore.Controllers
         {
             return View();
         }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
     }
 }
