a7d78c36cb2e3455c342a937537b6058ec6d1ee8
Soleks <SolekX@gmail.com>
2018-05-29 22:50:47 +0300

    Lesson_3: add controller, BL and BL.Context

---
+++ Lesson_3/WebStore/WebStore.BL.Context/IEmployeesData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Models;
+
+namespace WebStore.BL.Context
+{
+    public interface IEmployeesData
+    {
+        /// <summary>
+        /// Get employees
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<EmployeeView> GetAll();
+
+        /// <summary>
+        /// Get employee by id
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns></returns>
+        EmployeeView GetById(int id);
+
+        /// <summary>
+        /// Add new employee
+        /// </summary>
+        /// <param name="model"></param>
+        void AddNew(EmployeeView model);
+
+        /// <summary>
+        /// Remove 
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(int id);
+    }
+}
